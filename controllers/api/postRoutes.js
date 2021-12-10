0728d207eefa1147f760a93da0e15d335f13190a
Benjamin Clewer <benjiclewer@gmail.com>
2021-12-10 16:06:08 +1030

    updating routes, updating handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,25 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     const postData = await Post.getAll({
+//       // attributes: ['id', 'name', 'description']
+//       // include: [{model: Post}]
+//     });
+
+//     const posts = postData.map((post) => post.get({ plain: true }));
+
+//     res.render('post', {
+//       posts,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const profileRoute = require('./api/profileRoute')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile', profileRoute);
 
 module.exports = router;
